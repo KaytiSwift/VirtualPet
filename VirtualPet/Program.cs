0014fc5fefd74d6ee0841dc0d66af301c88b7fb8
KaytiSwift <kaytiswift@yahoo.com>
2018-09-21 14:35:16 -0400

    added Virtual Pet Class and User Interface

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,6 +10,29 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+
+            //Instantiates an instance of the VirtualPet class
+            VirtualPet pet = new VirtualPet();
+
+            //User Interface
+            Console.WriteLine("Ellen the Elephant");
+            Console.WriteLine("Hunger: {0}", pet.HungerLevel);
+            Console.WriteLine("Thirst: {0}", pet.ThirstLevel);
+            Console.WriteLine("Waste: {0}", pet.WasteLevel);
+            Console.WriteLine("Boredom: {0}", pet.BoredomLevel);
+            Console.WriteLine("Sickness: {0}", pet.SicknessLevel);
+            Console.WriteLine();
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1. Feed Ellen");
+            Console.WriteLine("2. Give Ellen a drink");
+            Console.WriteLine("3. Give Ellen a potty break");
+            Console.WriteLine("4. Play with Ellen");
+            Console.WriteLine("5. Take Ellen to the doctors.");
+            Console.WriteLine("6. Do nothing");
+
+
+
+
         }
     }
 }
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        //Fields
+        private int hungerLevel;
+        private int thirstLevel;
+        private int wasteLevel;
+        private int boredomLevel;
+        private int sicknessLevel;
+
+        //Properties
+        public int HungerLevel
+        {
+            get { return this.hungerLevel; }
+            set { this.hungerLevel = value; }
+        }
+
+        public int ThirstLevel
+        {
+            get { return this.thirstLevel; }
+            set { this.thirstLevel = value; }
+        }
+
+        public int WasteLevel
+        {
+            get { return this.wasteLevel; }
+            set { this.wasteLevel = value; }
+        }
+
+        public int BoredomLevel
+        {
+            get { return this.boredomLevel; }
+            set { this.boredomLevel = value; }
+        }
+
+        public int SicknessLevel
+        {
+            get { return this.sicknessLevel; }
+            set { this.sicknessLevel = value; }
+        }
+
+        //Constructor
+
+        public VirtualPet()
+        {
+            //default constructor
+        }
+
+        //Methods
+        public void GetFood()
+        {
+
+        }
+
+        public void GetWater()
+        {
+
+        }
+
+        public void GoPotty()
+        {
+
+        }
+
+        public void GoPlay()
+        {
+
+        }
+
+        public void GetDoctor()
+        {
+
+        }
+
+        public void Tick()
+        {
+
+        }
+    }
+}
