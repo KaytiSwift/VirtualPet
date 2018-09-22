c5fb54b2591ae83742d05edecc8190789334ed0e
KaytiSwift <kaytiswift@yahoo.com>
2018-09-22 16:56:43 -0400

    updates

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -13,26 +13,100 @@ namespace VirtualPet
 
             //Instantiates an instance of the VirtualPet class
             VirtualPet pet = new VirtualPet();
+            bool restart = true;
+            do //runs entire program once, prompts to play again or quit
+            {                
+                bool exit = false; 
 
-            //User Interface
-            Console.WriteLine("Ellen the Elephant");
-            Console.WriteLine("Hunger: {0}", pet.HungerLevel);
-            Console.WriteLine("Thirst: {0}", pet.ThirstLevel);
-            Console.WriteLine("Waste: {0}", pet.WasteLevel);
-            Console.WriteLine("Boredom: {0}", pet.BoredomLevel);
-            Console.WriteLine("Sickness: {0}", pet.SicknessLevel);
-            Console.WriteLine();
-            Console.WriteLine("What do you want to do?");
-            Console.WriteLine("1. Feed Ellen");
-            Console.WriteLine("2. Give Ellen a drink");
-            Console.WriteLine("3. Give Ellen a potty break");
-            Console.WriteLine("4. Play with Ellen");
-            Console.WriteLine("5. Take Ellen to the doctors.");
-            Console.WriteLine("6. Do nothing");
+                //Prompts the user to name the Elephant and saves input to Name
+                Console.WriteLine("Please enter a name for your elephant.");
+                string name = Console.ReadLine();
+                pet.Name = name;
 
+                do //loops the user interface
+                {
+                    //User Interface - displays pets current status, asks for an action, and gives list of choices  
+                    Console.WriteLine();
+                    Console.WriteLine("{0} the Elephant", pet.Name);
+                    Console.WriteLine();
+                    Console.WriteLine("Hunger level is at {0}", pet.Hunger);
+                    Console.WriteLine("Thirst level is at {0}", pet.Thirst);
+                    Console.WriteLine("Potty level is at {0}", pet.Potty);
+                    Console.WriteLine("Boredom level is at {0}", pet.Boredom);
+                    Console.WriteLine("Sadness level {0}", pet.Sadness);
+                    Console.WriteLine();
+                    pet.UrgentNeeds();
+                    Console.WriteLine("{0} is ready! What do you want to do?", pet.Name);
+                    Console.WriteLine();
+                    Console.WriteLine("Type \"feed\" to feed {0}.", pet.Name);
+                    Console.WriteLine("Type \"water\" to give {0} something to drink.", pet.Name);
+                    Console.WriteLine("Type \"potty\" to give {0} a potty break.", pet.Name);
+                    Console.WriteLine("Type \"play\" to play with {0}.", pet.Name);
+                    Console.WriteLine("Type \"nothing\" to do nothing.\n", pet.Name);
 
+                    //takes user input and decides action, calling methods
+                    string input = Console.ReadLine().ToLower();
 
+                    switch (input)
+                    {
+                        case "feed":
+                            pet.GetFood(); 
+                            break;
 
+                        case "water":
+                            pet.GetWater();
+                            break;
+
+                        case "potty":
+                            pet.GoPotty();
+                            break;
+
+                        case "play":
+                            pet.GoPlay();
+                            break;
+
+                        case "nothing":
+                            pet.IncreaseSadness();
+                            break;
+
+                        default:
+                            Console.WriteLine();
+                            Console.WriteLine("I did not understand you!");
+                            Console.WriteLine();
+                            break;
+                    }
+
+                    pet.Tick(); // calls Tick method
+
+                    Console.WriteLine("It's been an hour. Press enter to interact with {0} some more or type \"exit\" to quit", pet.Name);
+                    string reply = Console.ReadLine().ToLower();
+                    if (reply == "exit")
+                    {
+                        exit = true;
+                    }
+
+                } while (exit == false);
+
+                //Thanks the user for playing and asks if they want to play again. Restarts the loop if they type yes 
+                Console.WriteLine("Good job! {0} appreciates you!", pet.Name);
+                Console.WriteLine("Do you want to play with another pet? Type \"yes\" to play again.");
+                string response = Console.ReadLine().ToLower();
+                if (response == "yes")
+                {
+                    pet.Hunger = 0;
+                    pet.Thirst = 0;
+                    pet.Potty = 0;
+                    pet.Boredom = 1;
+                    pet.Sadness = 0;
+                }
+                else
+                {
+                    restart = false;
+                }
+            } while (restart == true);
+
+            Console.WriteLine("Thank you for playing!");
+                        
         }
     }
 }
--- VirtualPet/VirtualPet.cs
+++ VirtualPet/VirtualPet.cs
@@ -9,79 +9,182 @@ namespace VirtualPet
     class VirtualPet
     {
         //Fields
-        private int hungerLevel;
-        private int thirstLevel;
-        private int wasteLevel;
-        private int boredomLevel;
-        private int sicknessLevel;
+        private string name;
+        private int hunger = 0;    
+        private int potty = 0;
+        private int boredom = 1;
+        private int sadness = 0;
+        private int thirst = 0;
+        
 
         //Properties
-        public int HungerLevel
+        public string Name
         {
-            get { return this.hungerLevel; }
-            set { this.hungerLevel = value; }
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
         }
 
-        public int ThirstLevel
+        public int Potty
         {
-            get { return this.thirstLevel; }
-            set { this.thirstLevel = value; }
+            get { return this.potty; }
+            set { this.potty = value; }
         }
 
-        public int WasteLevel
+        public int Boredom
         {
-            get { return this.wasteLevel; }
-            set { this.wasteLevel = value; }
+            get { return this.boredom; }
+            set { this.boredom = value; }
         }
 
-        public int BoredomLevel
+        public int Sadness
         {
-            get { return this.boredomLevel; }
-            set { this.boredomLevel = value; }
+            get { return this.sadness; }
+            set { this.sadness = value; }
         }
 
-        public int SicknessLevel
+        public int Thirst
         {
-            get { return this.sicknessLevel; }
-            set { this.sicknessLevel = value; }
+            get { return this.thirst; }
+            set { this.thirst = value; }
         }
 
         //Constructor
-
         public VirtualPet()
         {
             //default constructor
         }
 
         //Methods
-        public void GetFood()
+        public void GetFood() //affects hunger, potty and sadness(if applicable)
         {
-
+            if (Hunger <= 0) 
+            {
+                Console.WriteLine();
+                Console.WriteLine("{0} is not hungry right now.", Name);
+            }
+            else 
+            {
+                Hunger = 0;
+                Potty = Potty + 1;
+                Console.WriteLine();
+                Console.WriteLine("{0} is no longer hungry! But it increased her need to go potty.", Name);
+            }
+            if (Sadness > 0) 
+            {
+                Sadness = Sadness - 1;
+            }
         }
 
-        public void GetWater()
+        public void GoPotty() //affects potty and sadness(if applicable)
         {
-
+            if (Potty <= 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("{0} does not need to go potty right now.", Name);
+            }
+            else
+            {
+                Potty = 0;
+                Console.WriteLine();
+                Console.WriteLine("{0} went potty!", Name);
+            }
+            if (Sadness > 0)
+            {
+                Sadness = Sadness - 1;
+            }
         }
 
-        public void GoPotty()
+        public void GoPlay() //affects boredom, thirst and sadness(if applicable)
         {
-
+            if (Boredom <= 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("{0} does not want to play right now.", Name);
+            }
+            else
+            {
+                Boredom = 0;
+                Thirst = Thirst + 1;
+                Console.WriteLine();
+                Console.WriteLine("{0} is happy you played with her! But it made her more thirsty.", Name);
+            }
+            if (Sadness > 0)
+            {
+                Sadness = Sadness - 1;
+            }
         }
 
-        public void GoPlay()
+        public void GetWater() //affects thirst and sadness(if applicable)
         {
-
+            if (Boredom <= 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("{0} is not thirsty right now.", Name);
+            }
+            else
+            {
+                Thirst = 0;
+                Potty = Potty + 1;
+                Console.WriteLine();
+                Console.WriteLine("{0}'s thirst is quenched! But it increased her need to go potty.", Name);
+            }
+            if (Sadness > 0)
+            {
+                Sadness = Sadness - 1;
+            }
         }
 
-        public void GetDoctor()
+        public void IncreaseSadness() //increases sadness
         {
-
+            Sadness = Sadness + 1;
+            Console.WriteLine();
+            Console.WriteLine("{0} needs you! You made {0} sad. You should interact with her next time to decrease her sadness.", Name);
+            Console.WriteLine();
         }
 
-        public void Tick()
+        public void Tick() //displays current levels and increases hunger, thirst, potty, and boredom
         {
+            Console.WriteLine();
+            Console.WriteLine("Hunger level is at {0}", Hunger);
+            Console.WriteLine("Thirst level is at {0}", Thirst);
+            Console.WriteLine("Potty level is at {0}", Potty);
+            Console.WriteLine("Boredom level is at {0}", Boredom);
+            Console.WriteLine("Sadness level is at {0}", Sadness);            
+            Console.WriteLine();
+            Hunger = Hunger + 1;
+            Thirst = Thirst + 1;
+            Potty = Potty + 1;
+            Boredom = Boredom + 1;
+            
+        }
 
+        public void UrgentNeeds() //displays notifications to the user when certain levels are too high
+        {
+            if (Hunger > 4)
+            {
+                Console.WriteLine("{0} is very hungry! You should feed her!", Name);
+                Console.WriteLine();
+            }
+            if (Potty > 4)
+            {
+                Console.WriteLine("{0} really needs to go potty! You should take her out!", Name);
+                Console.WriteLine();
+            }
+            if (Boredom > 4)
+            {
+                Console.WriteLine("{0} is very bored! You should play with her!", Name);
+                Console.WriteLine();
+            }
+            if (Thirst > 4)
+            {
+                Console.WriteLine("{0} is very thirsty! You should give her some water!", Name);
+                Console.WriteLine();
+            }
         }
     }
 }
